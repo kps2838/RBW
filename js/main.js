2e98888464732a5bd69262640ee41d047a0de7e0
kps2838 <97150053+kps2838@users.noreply.github.com>
2022-05-14 00:14:42 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,7 +8,41 @@ $(function(){
     $.stellar({
         horizontalScrolling: false,
         verticalScrolling: true,
-        verticalOffset: 50,
+        verticalOffset: 230,
     });
+
+
+  $("section").each(function () {
+    $(this).on("mousewheel DOMMouseScroll", function (e) {
+      e.preventDefault();
+      var delta = 0;
+      if (!event) event = window.event;
+      if (event.wheelDelta) {
+          delta = event.wheelDelta / 120;
+          if (window.opera) delta = -delta;
+      } else if (event.detail) delta = -event.detail / 3;
+
+      var moveTop = null;
+      if (delta < 0) {
+          if ($(this).next() != undefined) {
+              moveTop = $(this).next().offset().top;
+          }
+      } else {
+          if ($(this).prev() != undefined) {
+              moveTop = $(this).prev().offset().top;
+          }
+      }
+
+      $("html,body").stop().animate({
+          scrollTop: moveTop + 'px'
+      }, {
+          duration: 800, complete: function () {
+          }
+
+      });
+
+    });
+
+  });
     
 })
